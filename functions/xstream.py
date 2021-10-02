9b66e3ac339c20cc6c966e8f4eb46c1ff6f12d8d
Abhishek Verma <av4programming@gmail.com>
2021-10-02 13:09:44 +0530

    done

--- functions/xstream.py
+++ functions/xstream.py
@@ -8,9 +8,9 @@ def get_links(stream_url):
     print(url)
     res = requests.post(url,headers={ "referrer": stream_url})
     urls = json.loads(res.text).get('data')
-    # for i in range(len(urls)):
-    #     url = urls[i]
-    #     link = url.get('file')
-    #     r = requests.get(link, allow_redirects=False, headers={"referrer":"https://embedsito.com/"})
-    #     urls[i]["file"] = r.headers.get('Location')
+    for i in range(len(urls)):
+        url = urls[i]
+        link = url.get('file')
+        r = requests.get(link, allow_redirects=False, headers={"referrer":"https://embedsito.com/", "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"})
+        urls[i]["file"] = r.headers.get('Location')
     return urls
